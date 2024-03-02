9c4c59ec565ad4e048ef4ba83f4f5dfec92de50f
Kevin Velez <kvelezsalazar07@gmail.com>
2024-03-01 21:28:45 -0600

    controller update with functions that use .tpl forms with POST and index.php

--- controller/control.php
+++ controller/control.php
@@ -12,7 +12,7 @@ class Control {
     }
 
     public function framework_manager(){
-        $this->userAccountActions();
+        $this->userAccountActions(); // main function (could be changed)
     }
 
     public function getModel(){
@@ -29,9 +29,9 @@ class Control {
     }
 
     public function userAccountActions() {
-        if(isset($_REQUEST['login'])) {
+        if(isset($_REQUEST['login'])) { // form based
             $this->userLogin();
-        } else if(isset($_REQUEST['signup'])){
+        } else if(isset($_REQUEST['signup'])){ // form based
             $this->userSignUp();
         } else {
             $this->view->setDisplay('login.tpl');
@@ -39,7 +39,7 @@ class Control {
     }
 
     public function userLogin() {
-        $user_type = $_REQUEST['user_type'];
+        $user_type = $_REQUEST['user_type']; // form based
         switch ($user_type) {
             case 'Student':
                 $this->view->setDisplay('student.tpl');
@@ -61,6 +61,7 @@ class Control {
     }
 
     public function userSignUp() {
+        // could be extended.
         $this->view->setDisplay('signup.tpl');
     }
 }
